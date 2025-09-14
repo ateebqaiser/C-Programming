75dc05af01c757276933b907129d133cb003144c
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-15 02:00:19 +0500

    Add example of printf and scanf

---
+++ C_Basics/input_output.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h> 
+void main()
+{
+	int number1=10;
+	int number2;
+//	For output use printf() :
+	printf("Hello Ateeb\n");
+    printf("Value of number 1 : %d\n",number1);
+	
+//	For input use scanf() :
+	printf("Enter value of number 2 : ");
+	scanf("%d",&number2);
+	printf("Value of number 1 : %d",number2);
+    getch();
+
+}
