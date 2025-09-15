6fb7352b47f03bb8f2100e5597fe2cc3c5994f05
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-16 00:20:10 +0500

    Add examples of relational operators (==, !=, >, <, >=, <=)

---
+++ C_Operators/relational_operators.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int number1=10,number2=20;
+    printf("%d == %d : %d\n",number1,number2,number1==number2);
+    printf("%d != %d : %d\n",number1,number2,number1!=number2);
+    printf("%d > %d : %d\n",number1,number2,number1>number2);
+    printf("%d < %d : %d\n",number1,number2,number1<number2);
+    printf("%d >= %d : %d\n",number1,number2,number1>=number2);
+    printf("%d <= %d : %d\n",number1,number2,number1<=number2);
+}
