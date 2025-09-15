a180694b136411d538302f6b799621dd142a5e8e
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-16 00:25:36 +0500

    Add examples of logical operators (&&, ||, !)

---
+++ C_Operators/logical_operators.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    int number1=10,number2=20;
+    printf("%d\n",(number1==number2)||(number1<number2));
+    printf("%d\n",(number1==number2)||(number1<number2));
+    printf("%d \n",(!number1>number2));
+}
