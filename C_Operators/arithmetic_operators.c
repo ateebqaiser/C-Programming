222218bd5004225bc67eded68fea790a180ec922
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-16 00:16:34 +0500

    Add examples of arithmetic operators (+, -, *, /, %)

---
+++ C_Operators/arithmetic_operators.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int number1=10,number2=20;
+    printf("Addition of two numbers : %d\n",number1+number2);
+    printf("Subtraction of two numbers : %d\n",number1-number2);
+    printf("Multiplication of two numbers : %d\n",number1*number2);
+    printf("Division of two numbers : %d\n",number1/number2);
+    printf("Remainder of two numbers : %d\n",number1%number2);
+}
