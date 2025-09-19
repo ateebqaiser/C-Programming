dc7971f7c9a089935b16259a9a898352e6096835
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-19 10:05:00 +0500

    Add program to check pass or fail using if-else

---
+++ C_ControlStatements/Decision_Making_Statements/if-else_statement/if_else_pass_fail.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int marks;
+    printf("Enter student marks : ");
+    scanf("%d",&marks);
+    if(marks>=50){
+        printf("Pass");
+    }
+    else{
+        printf("Fail");
+    }
+}
